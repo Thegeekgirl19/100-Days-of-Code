90d71265763d65cf8a7749c3ff937f403b6de786
The Geek Girl <95872219+Thegeekgirl19@users.noreply.github.com>
2022-04-03 19:27:54 +0530

    Update variables.java

--- Day-1/variables.java
+++ Day-1/variables.java
@@ -19,6 +19,31 @@ public class variables {
 
         int sum = a + b;
         System.out.println("Sum of your inputs is " + sum);
+        /*
+        Rules for declaring a variable name
+
+        We can choose a name while declaring a Java variable if the following rules are followed:
+
+        1. Must not begin with a digit. (E.g., 1arry is an invalid variable)
+        2. Name is case sensitive. (Harry and harry are different)
+        3. Should not be a keyword (like Void).
+        4. White space is not allowed. (int Code With Harry is invalid)
+        5. Can contain alphabets, $character, _character, and digits if the other conditions are met.
+        
+        Data Types --> Primitive (intrinsic)
+                   --> Non Primitive (derived)
+                   
+        Primitive Data Types (8 types)
+        1. Byte -- 1 byte size
+        2. Short -- 2 byte size
+        3. Int -- 4 byte size
+        4. Float -- 4 byte size
+        5. Long -- 8 byte size (used for big int)
+        6. Double -- 8 byte size (used for high preceision decimal values)
+        7. Char -- 2 byte size
+        8. Boolean -- size depends on JVM
+
+        */
 
     }
 }
