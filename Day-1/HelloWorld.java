033ccc890781a7414bcf67fe8a1e3df29fd64947
The Geek Girl <95872219+Thegeekgirl19@users.noreply.github.com>
2022-04-03 19:08:57 +0530

    Update HelloWorld.java

--- Day-1/HelloWorld.java
+++ Day-1/HelloWorld.java
@@ -1,9 +1,27 @@
-import java.util.Scanner;
-public class Main {
+package com.learningjava; //packages are used to group similar classes
+
+import java.util.Scanner; //importing Scanner class
+public class Main { 
+    // In Java, every program must contain a class.
+    // The filename and name of the class should be the same.
+    // Here, we've created a class named "Main".
+    // It is the entry point to the application.
+
     public static void main(String args[]) {
+      // This is the main() method of java program, every program must contain a main method.
       Scanner sc=new Scanner(System.in);
       System.out.println("Please enter your name: ");
       String n=sc.nextLine();
       System.out.println("Hola peeps! I am " + n);
     }
 }
+
+/*
+Naming Conventions
+
+    For classes, we use Pascal Convention. The first and Subsequent characters from a word are capital letters (uppercase).
+    Example: Main, MyScanner, MyEmployee, CodeWithHarry
+
+    For functions and variables, we use camelCaseConvention. Here the first character is lowercase, and the subsequent characters are uppercase like myScanner, myMarks, CodeWithHarry
+
+*/
